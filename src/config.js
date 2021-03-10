9e8e318b26d22a024aabbeffe3d3863992558a58
James Kernan <jtkernan92@gmail.com>
2021-03-10 12:23:22 -0700

    deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    //API_ENDPOINT: `https://afternoon-sea-39441.herokuapp.com/`,
-    API_ENDPOINT: 'http://localhost:8000/',
+    API_ENDPOINT: `https://afternoon-sea-39441.herokuapp.com/`,
+    //API_ENDPOINT: 'http://localhost:8000/',
 }
